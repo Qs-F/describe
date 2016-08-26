bca6a1fdfde6641e57f606b68a7c08426c25ba5a
Qs-F <qsf@de-liker.com>
2016-08-27 01:00:50 +0900

    Add -init option

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,6 +10,23 @@ import (
 )
 
 func main() {
+	_, err := os.Open(".describe")
+	m := flag.Bool("init", false, "init option touch .describe file in current directory")
+	flag.Parse()
+	if *m == true && err != nil { // if err is nil, there is a file.
+		_, err := os.Create(".describe")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		} else {
+			fmt.Println("success!")
+			return
+		}
+		return
+	} else if *m == true && err == nil {
+		fmt.Println("There is a .describe file. Please remove it.")
+		return
+	}
 	wd, err := os.Getwd()
 	os.Chdir(wd)
 	if err != nil {
